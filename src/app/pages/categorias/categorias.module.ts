aa2d8f4966a3d7b4a919c8cbbbdb65e79015a11e
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2019-03-17 10:23:58 -0300

    Criaçao do formulario de ediçao e cadastro de categoria

--- src/app/pages/categorias/categorias.module.ts
+++ src/app/pages/categorias/categorias.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CategoriasRoutingModule } from './categorias-routing.module';
 import { ListaCategoriasComponent } from './lista-categorias/lista-categorias.component';
@@ -9,7 +10,8 @@ import { FormularioCategoriasComponent } from './formulario-categorias/formulari
   declarations: [ListaCategoriasComponent, FormularioCategoriasComponent],
   imports: [
     CommonModule,
-    CategoriasRoutingModule
+    CategoriasRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class CategoriasModule { }
--- src/app/pages/categorias/formulario-categorias/formulario-categorias.component.ts
+++ src/app/pages/categorias/formulario-categorias/formulario-categorias.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Categoria } from '../compartilhada/categoria.model';
+import { CategoriaService } from '../compartilhada/categoria.service';
+
+import { switchMap } from 'rxjs/operators';
+
+import toastr from 'toastr';
+
 
 @Component({
   selector: 'app-formulario-categorias',
   templateUrl: './formulario-categorias.component.html',
   styleUrls: ['./formulario-categorias.component.css']
 })
-export class FormularioCategoriasComponent implements OnInit {
+export class FormularioCategoriasComponent implements OnInit, AfterContentChecked {
 
-  constructor() { }
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  categoria: Categoria = new Categoria();
+
+  constructor(
+    private categoriaService: CategoriaService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+    ) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCategoriaForm();
+    this.loadCategoria();
   }
 
+  ngAfterContentChecked() {
+    this.setPageTitle();
+  }
+
+  private setCurrentAction() {
+    if (this.route.snapshot.url[0].path === 'criar') {
+      this.currentAction = 'criar';
+    } else {
+      this.currentAction = 'editar';
+    }
+  }
+
+  private buildCategoriaForm() {
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      nome: [null, [Validators.required, Validators.minLength(2)]],
+      descricao: [null]
+    });
+  }
+
+  private loadCategoria() {
+    if (this.currentAction === 'editar') {
+      this.route.paramMap.pipe(
+        switchMap(params => this.categoriaService.getById(+params.get('id')))
+      ).subscribe(
+        (categoria) => {
+          this.categoria = categoria;
+          this.categoryForm.patchValue(categoria); // Passa os dados da categoria carregada para o categoria form
+        },
+        (error) => alert('Ocorreu um erro no servidor, tente mais tarde.')
+      );
+    }
+  }
+
+  private setPageTitle() {
+    if (this.currentAction === 'criar') {
+      this.pageTitle = 'Cadastro de nova categoria';
+    } else {
+      const categoriaName = this.categoria.nome || '';
+      this.pageTitle = 'Editando categoria: ' + categoriaName;
+    }
+  }
 }
