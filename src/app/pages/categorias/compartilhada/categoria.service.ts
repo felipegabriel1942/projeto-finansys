571c175b7258aa85e825df5f2f36928784ead29d
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2019-03-23 11:08:42 -0300

    Refatoração completa da classe de serviço de categoria

--- src/app/pages/categorias/compartilhada/categoria.service.ts
+++ src/app/pages/categorias/compartilhada/categoria.service.ts
@@ -1,72 +1,17 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-import { Observable, throwError } from 'rxjs';
-import { map, catchError, flatMap } from 'rxjs/operators';
+import { Injectable, Injector } from '@angular/core';
 
 import { Categoria } from './categoria.model';
+
+import { BaseResourceService } from '../../../shared/servicos/base-resource.service';
+
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoriaService {
+export class CategoriaService extends BaseResourceService<Categoria> {
 
-  private apiPath: string = 'api/categorias';
-
-  constructor(private http: HttpClient) { }
-
-  getAll(): Observable<Categoria[]> {
-    return this.http.get(this.apiPath).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToCategorias)
-    );
-  }
-
-  getById(id: number): Observable<Categoria> {
-    const url = `${this.apiPath}/${id}`;
-    return this.http.get(url).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToCategoria)
-    );
-  }
-
-  create(categoria: Categoria): Observable<Categoria> {
-    return this.http.post(this.apiPath, categoria).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToCategoria)
-    );
-  }
-
-  update(categoria: Categoria): Observable<Categoria> {
-    const url = `${this.apiPath}/${categoria.id}`;
-    return this.http.put(url, categoria).pipe(
-      catchError(this.handleError),
-      map(() => categoria)
-    );
-  }
-
-  delete(id: number): Observable<any> {
-    const url = `${this.apiPath}/${id}`;
-    return this.http.delete(url).pipe(
-      catchError(this.handleError),
-      map(() => null)
-    );
-  }
-
-  // metodos privados
-  private jsonDataToCategorias(jsonData: any[]): Categoria[] {
-    const categorias: Categoria[] = [];
-    jsonData.forEach(element => categorias.push(element as Categoria));
-    return categorias;
-  }
-
-  private jsonDataToCategoria(jsonData: any): Categoria {
-    return jsonData as Categoria;
-  }
-
-  private handleError(error: any): Observable<any> {
-    console.log('ERRO NA REQUISIÇÃO =>', error);
-    return throwError(error);
-  }
+  constructor(protected injector: Injector) {
+    super('api/categorias', injector);
+   }
 }
