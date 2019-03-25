7303a9be81a21cfd77be06831e48d63faa49dabd
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2019-03-25 10:10:49 -0300

    Padronizado componente de bread-crumb

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { BreadCrumbComponent } from './componentes/bread-crumb/bread-crumb.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [BreadCrumbComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   exports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule,
+    BreadCrumbComponent
   ]
 })
 export class SharedModule { }
